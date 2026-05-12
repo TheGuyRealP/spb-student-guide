283a46327278743363d3f75fd2c492535395f584
Peter Simwanza <103517249+TheGuyRealP@users.noreply.github.com>
2026-05-12 18:07:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/* =====================================================
+======================= script.js ======================
+===================================================== */
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+if (toggleBtn) {
+
+  toggleBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+      toggleBtn.textContent = "☀️";
+    } else {
+      toggleBtn.textContent = "🌙";
+    }
+
+  });
+
+}
